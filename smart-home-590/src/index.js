959e02358ce16c67d8ba85000fed575117e62090
Sara-G30 <sgaber@stevens.edu>
2024-12-03 11:22:13 -0500

    initial app setup --frontend

---
+++ smart-home-590/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './pages/home.js'
+import Login from './pages/login.js'
+import Layout from './pages/layout.js';
+import Register from './pages/register.js';
+import Settings from './pages/settings.js';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Register />} />
+      <Route path="login" element={<Login />} />
+      <Route path="home" element={<Home />} />
+      <Route path="settings" element={<Settings />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ smart-home-590/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["emerald", "dim"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+  },
+}
+
